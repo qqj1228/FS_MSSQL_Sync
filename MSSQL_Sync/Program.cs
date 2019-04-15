05813d570b259a92333d0a7fda7e7d62808550d7
qqj1228 <qqj1228@gmail.com>
2019-04-15 15:45:24 +0800

    添加项目文件。

---
+++ MSSQL_Sync/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSSQL_Sync {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
